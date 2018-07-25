b4aa006f10cc0e49b6a37ba37efaf5c5a0d92a4c
vadns <vadns@fastmail.com>
2018-07-25 08:33:28 +0800

    initial version: 0.0.1 (unstable)
    - add CMakeLists.txt skeleton
    - add version number support

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+
+#include <vadns/version.h>
+#include <cstdio>
+
+int main(int argc, char** argv)
+{
+    printf("version %d.%d.%d (%s)\ngit commit %s from branch %s\n",
+        VADNS_VERSION_MAJOR, VADNS_VERSION_MINOR, VADNS_VERSION_PATCH, VADNS_VERSION_SUFFIX,
+        VADNS_GIT_COMMIT_HASH, VADNS_GIT_BRANCH);
+
+    return 0;
+}
